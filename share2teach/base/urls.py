5c1e3ef9b81eb1a3392a2ab6c9a87e8e4261ee33
Bonolo Zizi <bonolozizi7@gmail.com>
2024-08-14 16:29:34 +0200

    Our django files

---
+++ share2teach/base/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path 
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", views.home, name="home"), 
+    path("register/", views.register, name="register" ),
+    path("login/", views.login, name="login")
+]
--- share2teach/base/views.py
+++ share2teach/base/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import login as auth_login
+from django.conf import settings
+from django.shortcuts import redirect
+from django.views import View
+
+def home(request):
+    return render(request, 'home.html')
 
 # Create your views here.
+from django.shortcuts import render
+
+def register(request):
+    # Your registration logic here
+    return render(request, 'register.html')
+
+def login(request):
+    # Your registration logic here
+    return render(request, 'login.html')
